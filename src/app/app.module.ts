fe60d223057301016343e62a6a6e03b0be15357b
shahsiddhant <siddhant@siddhant.design>
2020-07-17 11:41:33 -0600

    incorporate indexeddb for data storage, add api for crud articles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { NgxIndexedDBModule } from 'ngx-indexed-db';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ArticleListComponent } from './article-list/article-list.component';
 import { ArticleListItemComponent } from './article-list-item/article-list-item.component';
+import { ArticleListComponent } from './article-list/article-list.component';
+import { dbConfig } from './db-config';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ArticleListComponent,
-    ArticleListItemComponent
-  ],
+  declarations: [AppComponent, ArticleListComponent, ArticleListItemComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxIndexedDBModule.forRoot(dbConfig),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/db-config.ts
@@ -0,0 +1,31 @@
+import { DBConfig } from 'ngx-indexed-db';
+
+export const dbConfig: DBConfig = {
+  name: 'MyDb',
+  version: 1,
+  objectStoresMeta: [
+    {
+      store: 'articles',
+      storeConfig: { keyPath: 'id', autoIncrement: true },
+      storeSchema: [
+        { name: 'title', keypath: 'title', options: { unique: false } },
+        { name: 'author', keypath: 'author', options: { unique: false } },
+        {
+          name: 'publishedOn',
+          keypath: 'publishedOn',
+          options: { unique: false },
+        },
+        {
+          name: 'lastUpdated',
+          keypath: 'lastUpdated',
+          options: { unique: false },
+        },
+        {
+          name: 'body',
+          keypath: 'body',
+          options: { unique: false },
+        },
+      ],
+    },
+  ],
+};
