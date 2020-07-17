a131d83f2944564494ccd0c89bc44fcd1fad0fd9
shahsiddhant <siddhant@siddhant.design>
2020-07-17 16:28:26 -0600

    add quill

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,26 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgxIndexedDBModule } from 'ngx-indexed-db';
+import { QuillModule } from 'ngx-quill';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ArticleDetailComponent } from './article-detail/article-detail.component';
 import { ArticleListItemComponent } from './article-list-item/article-list-item.component';
 import { ArticleListComponent } from './article-list/article-list.component';
 import { dbConfig } from './db-config';
-import { ArticleDetailComponent } from './article-detail/article-detail.component';
-
 @NgModule({
-  declarations: [AppComponent, ArticleListComponent, ArticleListItemComponent, ArticleDetailComponent],
+  declarations: [
+    AppComponent,
+    ArticleListComponent,
+    ArticleListItemComponent,
+    ArticleDetailComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
     NgxIndexedDBModule.forRoot(dbConfig),
+    QuillModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent],
